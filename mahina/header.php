8db6867aa941cb2f74f9c7e0b57b1fd3194c1ad4
Krankrestey <opengithub@yandex.ru>
2014-12-17 23:04:37 +0600

    background
    +

--- mahina/header.php
+++ mahina/header.php
@@ -16,7 +16,7 @@
      <tr>
 	   <td colspan=4 align=right> <a href="/~mahina/register.php"> Зарегистрироваться </a> </td>
 	 </tr>'; ?>
-	 <tr height="50px" size=4 style="background:#0A67A3; font-weight:500;">
+	 <tr height="50px" size=4 style="background:rgba(120,120,255,0.7); font-weight:500;">
        <td width=25% align=center> <a href="/~mahina"> <font size=4> Главная </font> </a> </td>
 	   <td width=25% align=center> <a href="/~mahina/blog.php"> <font size=4> Новости </font> </a> </td>
 	   <td width=25% align=center> <a href="/~mahina/talk.php"> <font size=4> Сжатие данных </font> </a> </td>
