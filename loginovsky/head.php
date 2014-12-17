2ef29e7b671ad88813143d624fcf577578ee9ea7
Krankrestey <opengithub@yandex.ru>
2014-12-18 00:45:08 +0600

    added style
    rfhjx yt vjue ,tp cnbktq

--- loginovsky/head.php
+++ loginovsky/head.php
@@ -1,18 +1,18 @@
 <nav>
 <ul>
-<a href="index.php"><li>βμοη</li></a>
-<a href="talk.php" class="right"><li>δολμαδ</li></a>
-<a href="hobby.php" class="right"><li>θοββι</li></a>
+<a href="ccfit.nsu.ru/~loginovsky/index.php"><li>βμοη</li></a>
+<a href="ccfit.nsu.ru/~loginovsky/talk.php" class="right"><li>δολμαδ</li></a>
+<a href="ccfit.nsu.ru/~loginovsky/hobby.php" class="right"><li>θοββι</li></a>
 
 <?php
 if(!isset($_COOKIE['logged'])){
-	        echo '<a href="login.php"><li class="right">χοκτι</li></a>';
+	        echo '<a href="ccfit.nsu.ru/~loginovsky/login.php"><li class="right">χοκτι</li></a>';
 }
 else
 {
 	        $user=substr(rtrim($_COOKIE['logged']),0,-32);
-		        echo '<a href="vyhod.php"><li class="right">χωκτι</li></a>';
-		        echo '<a href="addnote.php"><li class="right">δοβαχιτψ ποστ</li></a>';
+		        echo '<a href="ccfit.nsu.ru/~loginovsky/vyhod.php"><li class="right">χωκτι</li></a>';
+		        echo '<a href="ccfit.nsu.ru/~loginovsky/addnote.php"><li class="right">δοβαχιτψ ποστ</li></a>';
 			        echo '<a href=#><li class="right">πςιχετ,'.$user.'!</li></a>';
 }
 ?>
