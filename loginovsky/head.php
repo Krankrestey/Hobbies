8f2eae8d4826be58f52866109e238d7683809597
Krankrestey <opengithub@yandex.ru>
2014-12-18 00:58:43 +0600

    little changes
    gdsdzfg

--- loginovsky/head.php
+++ loginovsky/head.php
@@ -1,8 +1,8 @@
 <nav>
 <ul>
-<a href="ccfit.nsu.ru/~loginovsky/index.php"><li>βμοη</li></a>
-<a href="ccfit.nsu.ru/~loginovsky/talk.php" class="right"><li>δολμαδ</li></a>
-<a href="ccfit.nsu.ru/~loginovsky/hobby.php" class="right"><li>θοββι</li></a>
+<a href="/~loginovsky/index.php"><li>βμοη</li></a>
+<a href="/~loginovsky/talk.php" class="right"><li>δολμαδ</li></a>
+<a href="/~loginovsky/hobby.php" class="right"><li>θοββι</li></a>
 
 <?php
 if(!isset($_COOKIE['logged'])){
@@ -11,8 +11,8 @@ if(!isset($_COOKIE['logged'])){
 else
 {
 	        $user=substr(rtrim($_COOKIE['logged']),0,-32);
-		        echo '<a href="ccfit.nsu.ru/~loginovsky/vyhod.php"><li class="right">χωκτι</li></a>';
-		        echo '<a href="ccfit.nsu.ru/~loginovsky/addnote.php"><li class="right">δοβαχιτψ ποστ</li></a>';
+		        echo '<a href="/~loginovsky/vyhod.php"><li class="right">χωκτι</li></a>';
+		        echo '<a href="/~loginovsky/addnote.php"><li class="right">δοβαχιτψ ποστ</li></a>';
 			        echo '<a href=#><li class="right">πςιχετ,'.$user.'!</li></a>';
 }
 ?>
